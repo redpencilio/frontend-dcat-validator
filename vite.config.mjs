7174062c5dd896db2c9dac7d4f7ce6c22c3a43f4
Niels V <niels.vandekeybus@redpencil.io>
2026-04-24 10:45:36 +0200

    set up local routing to backend

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,19 @@ import { defineConfig } from 'vite';
 import { extensions, classicEmberSupport, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 
+const LOCAL_PATHS = [
+  /^\/(\?.*)?$/,
+  /^\/index\.html/,
+  /^\/@vite/,
+  /^\/vite/,
+  /\/[-@]?embroider/,
+  /^\/app\//,
+  /^\/assets/,
+  /^\/node_modules\//,
+  /^\/vendor\//,
+  /^\/component-library/,
+];
+
 export default defineConfig({
   plugins: [
     classicEmberSupport(),
@@ -12,4 +25,26 @@ export default defineConfig({
       extensions,
     }),
   ],
+  server: {
+    proxy: {
+      '/': {
+        target: 'http://localhost',
+        changeOrigin: false,
+        secure: false,
+        ws: true,
+        bypass(req) {
+          if (
+            req.headers.upgrade === 'websocket' &&
+            req.headers['sec-websocket-protocol'] === 'vite-hmr'
+          ) {
+            return req.url;
+          }
+          if (LOCAL_PATHS.some((re) => re.test(req.url))) {
+            return req.url;
+          }
+          return null;
+        },
+      },
+    },
+  },
 });
