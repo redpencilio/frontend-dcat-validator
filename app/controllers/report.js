868f8224e028e4bf196af8fc0ae9bc65efdd8609
Ruben Simons <rubensimons442@gmail.com>
2026-08-19 11:45:58 +0200

    Integrate

--- app/controllers/report.js
+++ app/controllers/report.js
@@ -4,13 +4,9 @@ import { tracked } from '@glimmer/tracking';
 
 export default class ReportController extends Controller {
   @tracked errorMessage = null;
-  @tracked expandedGroup = null;
   @tracked reportDate = null;
   @tracked latestReportId = null;
   @tracked latestReportDate = null;
+  @tracked vocabReport = null;
 
-  @action
-  toggleGroup(type) {
-    this.expandedGroup = this.expandedGroup === type ? null : type;
-  }
 }
